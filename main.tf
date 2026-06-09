bb32a0e7d518337d51cb1a0d6fedf90dcb3ebf7d
Milky19 <97377003+Milky19@users.noreply.github.com>
2026-06-09 08:15:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,62 +2,65 @@
 provider "aws" {
 region = "us-east-1"
 }
-
 resource "aws_instance" "one" {
-  ami             = "ami-0152204c1a187337c"
-  instance_type   = "t3.micro"
-  key_name        = "DOCKER"
+  ami                    = "ami-0152204c1a187337c"
+  instance_type          = "t3.micro"
+  key_name               = "DOCKER"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1a"
-  user_data       = <<EOF
+  availability_zone      = "us-east-1a"
+
+  user_data = <<EOF
 #!/bin/bash
-sudo -i
 yum install httpd -y
 systemctl start httpd
-chkconfig httpd on
-echo "This is my app created by terraform infrastructurte by pythonlife devops krishna sir SERVER-1" > /var/www/html/index.html
+systemctl enable httpd
+echo "This is my app created by Terraform - Server 1" > /var/www/html/index.html
 EOF
+
   tags = {
     Name = "web-server-1"
   }
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-0152204c1a187337c"
-  instance_type   = "t3.micro"
-  key_name        = "DOCKER"
+  ami                    = "ami-0152204c1a187337c"
+  instance_type          = "t3.micro"
+  key_name               = "DOCKER"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1b"
-  user_data       = <<EOF
+  availability_zone      = "us-east-1b"
+
+  user_data = <<EOF
 #!/bin/bash
-sudo -i
 yum install httpd -y
 systemctl start httpd
-chkconfig httpd on
-echo "HI all this is my website created by terraform infrastructurte by Pythonlife Krishna sir server-2" > /var/www/html/index.html
+systemctl enable httpd
+echo "This is my app created by Terraform - Server 2" > /var/www/html/index.html
 EOF
+
   tags = {
     Name = "web-server-2"
   }
 }
 
 resource "aws_instance" "three" {
-  ami             = "ami-0152204c1a187337c"
-  instance_type   = "t3.micro"
-  key_name        = "DOCKER"
+  ami                    = "ami-0152204c1a187337c"
+  instance_type          = "t3.micro"
+  key_name               = "DOCKER"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1a"
+  availability_zone      = "us-east-1a"
+
   tags = {
     Name = "app-server-1"
   }
 }
 
 resource "aws_instance" "four" {
-  ami             = "ami-0152204c1a187337c"
-  instance_type   = "t3.micro"
-  key_name        = "DOCKER"
+  ami                    = "ami-0152204c1a187337c"
+  instance_type          = "t3.micro"
+  key_name               = "DOCKER"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "us-east-1b"
+  availability_zone      = "us-east-1b"
+
   tags = {
     Name = "app-server-2"
   }
@@ -65,6 +68,7 @@ resource "aws_instance" "four" {
 
 resource "aws_security_group" "five" {
   name = "elb-sg"
+
   ingress {
     from_port   = 22
     to_port     = 22
@@ -86,12 +90,13 @@ resource "aws_security_group" "five" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 resource "aws_ebs_volume" "eight" {
- availability_zone = "us-east-1a"
-  size = 25
+  availability_zone = "us-east-1a"
+  size              = 25
+
   tags = {
     Name = "ebs-001"
   }
 }
-
 
